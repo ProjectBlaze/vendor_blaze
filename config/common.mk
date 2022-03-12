1d14032a2836dd9d26e13b359814c6ee6fbe6f55
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-03-12 23:56:55 +0000

    vendor: Add FaceUnlock
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -170,4 +170,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/blaze/build/target/product/security/blaze
 
+#Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 include vendor/blaze/config/version.mk
