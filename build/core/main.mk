6d04e7df5bfdea2b30d75ae1234dcb68d56b6d5c
Aryan Sinha <sinha.aryan03@gmail.com>
2022-03-21 05:15:16 +0000

    blaze: Show Android version as 12L [2/2]
    * Android 12 & 12L, Both Show Platform version as "12", while 12L is not a QPR release , its a Android Bump
      ( Android 12 = VNDK31, Android 12.1/12L = VNDK 32 ), Even Google released the newer tags with android-12.1.0_r1 ,
       not with android-12.0.0 branding [1], Some end users do not know if they're running Android 12 or 12L, this's because
       Android Version Being displayed as "12" on both 12 & 12L, Even Google Docs [2] refer the newer Android Version as 12L,
       I've no idea why they didnt care to bump it here, hence refactor PLATFORM_VERSION_LAST_STABLE to 12L
    
    [1] - https://android.googlesource.com/platform/manifest/+/refs/tags/android-12.1.0_r1
    [2] - https://developer.android.com/about/versions/12/12L/summary
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,2 @@
-# Include LineageOS versions
-include $(TOPDIR)vendor/lineage/build/core/main_version.mk
+# Include ProjectBlaze versions
+include $(TOPDIR)vendor/blaze/build/core/main_version.mk
--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,25 +1,3 @@
-# Build fingerprint
-ifneq ($(BUILD_FINGERPRINT),)
+# ProjectBlaze Android 12L Specific Props
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-endif
-
-# LineageOS System Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
-
-# LineageOS Platform SDK Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.blaze.settings.android_version=12L
