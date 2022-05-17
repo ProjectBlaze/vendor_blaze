f10d7cd60796a284b816b1e3eb5fb517775f5f28
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-05-17 21:44:03 +0530

    vendor: Fix Boot Animation Resolutions
    *For 720p & 1440p Display devices.
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -3,13 +3,11 @@ TARGET_BOOT_ANIMATION_RES ?= undefined
 ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
      PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/2160.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2560)
-     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/2560.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using 1080p bootanimation )
      PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
