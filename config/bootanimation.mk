7dca7b25413d42145bb73326b83207cfeab7b826
afterallafk <shivamatiet2001@gmail.com>
2022-05-27 19:46:40 +0000

    Add ProjectBlaze BootAnimation
    Thanks @tdotk (telegram username) for your hardwork and providing us the boot animation
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

---
+++ config/bootanimation.mk
@@ -0,0 +1,17 @@
+TARGET_BOOT_ANIMATION_RES ?= undefined
+
+ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
+     $(warning Target bootanimation res is undefined, using 1080p bootanimation )
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+     $(warning Defined bootanimation res is wrong, using 1080p bootanimation )
+     PRODUCT_COPY_FILES += vendor/blaze/prebuilt/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+endif
