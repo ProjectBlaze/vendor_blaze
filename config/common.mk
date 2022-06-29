519888c9236d0246f37e5d89bf1b507b9f2387a1
spkal01 <kalligeross@gmail.com>
2022-06-29 20:34:55 +0530

    Build Lawnchair by default

--- config/common.mk
+++ config/common.mk
@@ -221,3 +221,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+# Lawnchair 
+TARGET_BUILD_LAWNCHAIR ?= true
+ifeq ($(strip $(TARGET_BUILD_LAWNCHAIR)),true)
+include vendor/lawnchair/lawnchair.mk
+endif
