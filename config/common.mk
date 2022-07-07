b0b933fc5746a8fa2ce7c42fa51f741decf7c057
Beru Hinode <windowz414@1337.lgbt>
2022-07-07 17:52:00 +0530

    vendor: Add our ringtone and set as default
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -24,6 +24,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Audio
+$(call inherit-product, vendor/blaze/audio/audio.mk)
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -213,3 +216,7 @@ PRODUCT_COPY_FILES += \
 endif
 
 include vendor/blaze/config/version.mk
+
+# Sounds (default)
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=vibe.ogg
