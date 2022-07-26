b670584c3fbbccfa1516eb4d5e676b186360db96
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-07-26 10:38:42 +0530

    Update GAPPS calling command

--- config/common.mk
+++ config/common.mk
@@ -118,7 +118,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product, vendor/gms/products/gms.mk)
 endif
 
 # Optimize everything for preopt
