1cc7c13768aa212e8794289cd24d96b90ba25984
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-08-02 23:58:10 +0530

    Nuke lawnchair

--- config/common.mk
+++ config/common.mk
@@ -226,11 +226,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# Lawnchair
-ifneq ($(WITH_GAPPS), true)
-include vendor/lawnchair/lawnchair.mk
-endif
-
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=vibe.ogg
