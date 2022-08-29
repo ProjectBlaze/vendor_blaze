7566570bd03cd39157fb6a51f21f874e53a55599
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:44:38 +0300

    config: Remove TARGET_HAS_MEMFD_BACKPORT
    Change-Id: I17444007429f826bf3d7504cba760204c476e664

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -35,7 +35,6 @@ SOONG_CONFIG_blazeGlobalVars += \
     camera_needs_client_info_lib \
     gralloc_handle_has_reserved_size \
     has_legacy_camera_hal1 \
-    has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
@@ -64,7 +63,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_blazeGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_blazeGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
-SOONG_CONFIG_blazeGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_blazeGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
