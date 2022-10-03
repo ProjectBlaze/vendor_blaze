d9e41aee53e26ead343b5a9415a06e1ed5099f64
Bedirhan KURT <windowz414@gnuweeb.org>
2022-10-03 07:50:25 +0000

    config: Set default alarm sound
    *Idea taken from ProjectKasumi
    Signed-off-by: rdx420 <padraramesh420@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -222,4 +222,5 @@ include vendor/blaze/config/version.mk
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=vibe.ogg \
-    ro.config.notification_sound=Pikachu.ogg
+    ro.config.notification_sound=Pikachu.ogg \
+    ro.config.alarm_alert=MorningAlarm.ogg
