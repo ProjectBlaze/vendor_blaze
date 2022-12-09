f76167c38d2b20231b69f7647761d3b4984959c3
Anay Wadhera <anay1018@gmail.com>
2022-12-09 12:51:26 +0900

    vendor: Adapt clock font to qpr1
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,6 +1,6 @@
 # Fonts
 PRODUCT_COPY_FILES += \
       vendor/blaze/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-      vendor/blaze/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \ 
+      vendor/blaze/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+      vendor/blaze/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \ 
       vendor/blaze/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
-
