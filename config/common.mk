9516fc3071758749d446252da243b3847a1106b3
Pranav Vashi <neobuddy89@gmail.com>
2023-01-10 21:01:07 +0530

    config: Minimize debug info
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I21c31ce111c393ed497be3c7396980e2b2b78afe

--- config/common.mk
+++ config/common.mk
@@ -137,6 +137,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+WITH_DEXPREOPT_DEBUG_INFO := false
 
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
