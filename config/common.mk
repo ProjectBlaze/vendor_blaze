495fed5fac523a2f3cd3375aeffafb00d2b2f3e2
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2023-03-03 14:06:20 +0530

    vendor: config: Unset pikachu notification sound
    *As some people find it attractive and some might find it annoying. So, disable it by default and let them choose from menu.

--- config/common.mk
+++ config/common.mk
@@ -214,5 +214,4 @@ include vendor/blaze/config/version.mk
 # Sounds (default)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=vibe.ogg \
-    ro.config.notification_sound=Pikachu.ogg \
     ro.config.alarm_alert=MorningAlarm.ogg
