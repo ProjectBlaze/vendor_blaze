bf3a940ece01d9cbdb797e20741ffd296d0923f2
Danny Trunk <dtrunk90@gmail.com>
2023-04-22 10:57:14 +0000

    soong: Add TARGET_USES_EGL_DISPLAY_ARRAY conditional
    This soong variable is used to conditionally revert a commit [1]
    which causes random camera crashes on tama devices
    
    [1] LineageOS/android_frameworks_native@a9550f3
    
    Change-Id: I749c7029f8f2b6d6d95b066aed4929c33e3c75e7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -38,7 +38,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
     uses_legacy_fd_fbdev \
-    needs_oplus_tag
+    needs_oplus_tag \
+    uses_egl_display_array
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -59,6 +60,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_blazeGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_blazeGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_blazeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
