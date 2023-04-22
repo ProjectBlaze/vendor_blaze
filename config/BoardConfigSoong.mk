29ea41d87b19217e2894f552f9558b2bbd94e10e
inferno0230 <adityachoudhury0230@gmail.com>
2023-04-22 10:53:50 +0000

    soong: Add TARGET_NEEDS_OPLUS_VENDOR_TAG
    Change-Id: Ica0675e3b812e32459d82ea3d648829a73f26be5
    (cherry picked from commit 11f751a)

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -47,7 +47,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    needs_oplus_tag
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -69,6 +70,7 @@ SOONG_CONFIG_blazeGlobalVars_gralloc_handle_has_custom_content_md_reserved_size 
 SOONG_CONFIG_blazeGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_blazeGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_blazeNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_blazeGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_blazeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
