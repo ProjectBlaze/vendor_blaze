4d05fbcc92bef654c7787114940a836a6f85ec6b
aswin7469 <aswinas@pixysos.com>
2023-06-15 18:38:50 +0530

    fonts: Update from TQ3A.230605.012
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -2,5 +2,6 @@
 PRODUCT_COPY_FILES += \
       vendor/blaze/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
       vendor/blaze/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-      vendor/blaze/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \ 
+      vendor/blaze/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+      vendor/blaze/fonts/GoogleSansFlex-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansFlex-Regular.ttf \
       vendor/blaze/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
