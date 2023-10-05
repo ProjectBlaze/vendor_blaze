52f358253d1f699a911090bd9e18fe430ddfa394
afterallafk <shivamatiet2001@gmail.com>
2023-10-05 09:50:24 +0530

    vendor: Getting ready for Android 14
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ANDROID_VERSION := 13
-BLAZEVERSION := v2.8
+ANDROID_VERSION := 14
+BLAZEVERSION := V3.0-UNEARTH
 
 BLAZE_BUILD_TYPE ?= UNOFFICIAL
 BLAZE_MAINTAINER ?= UNKNOWN
