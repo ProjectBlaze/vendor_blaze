PixelExperience/vendor_aosp

vendor: Import clocks package
Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf

config/common.mk
@@ -219,6 +219,9 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Clocks
+$(call inherit-product, vendor/aosp/config/clocks.mk)
+
 # Pixel Framework
 #$(call inherit-product, vendor/pixel-framework/config.mk)
 

ProjectBlaze/vendor_blaze

Signed-off-by: itsxrp <itsxrproms@gmail.com>

config/common.mk
@@ -225,6 +225,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.wm.enable_remote_keyguard_animation=0
 
+# Clocks
+$(call inherit-product, vendor/blaze/config/clocks.mk)
+
 # Clean up packages cache to avoid wrong strings and resources
 PRODUCT_COPY_FILES += \
     vendor/blaze/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh