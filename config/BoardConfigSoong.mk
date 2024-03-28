196f017bffdb2b19568234777ce2b127d582af45
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2024-03-28 10:11:51 +0000

    vendor: soong: Add TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB
    Recently LOS changed KeyHandler to service. Service doesn't use system_server context, so FileObserver of old implementation can't access client info file. This implementation directly writes package name without using any files.
    
    Signed-off-by: Shantanu Sarkar <shantanuplussarkar@gmail.com>
    Change-Id: Ie69bac2adb8150f6954730907a9ec447877647cb
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_blazeGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_override_format_from_reserved \
+    camera_needs_client_info_lib \
     gralloc_handle_has_custom_content_md_reserved_size \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
@@ -74,6 +75,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_blazeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
+SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_blazeGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_blazeGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_blazeGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
